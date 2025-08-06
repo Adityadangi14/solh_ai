ca3ad7400bfb5723f122f33789777c14d4df6dd8
Adityadangi14 <dangiaditya@gmail.com>
2025-08-06 11:05:22 +0530

    components updated

--- renderer/components.go
+++ renderer/components.go
@@ -55,12 +55,19 @@ func RenderTypeController(obj appmodels.ContentModel) map[string]any {
 
 	case "audio":
 		return RenderAudio(obj)
+
+	case "pdf":
+		return RenderPdf(obj)
+
+	case "Video":
+		return RenderVideo(obj)
+
 	default:
 		return map[string]any{}
 	}
 }
 
-func RenderBlog(model appmodels.ContentModel) map[string]any {
+func RenderPdf(model appmodels.ContentModel) map[string]any {
 	str := `
 	    {
   "type": "container",
@@ -141,6 +148,129 @@ func RenderBlog(model appmodels.ContentModel) map[string]any {
                       {"type": "sizedBox", "height": 5},
                       {
                         "type": "text",
+                        "data": "PDF",
+                        "style": {"fontSize": 12, "color": "#FFFFFF"}
+                      },
+                      {"type": "sizedBox", "height": 5}
+                    ]
+                  },
+                  {"type": "sizedBox", "width": 5}
+                ]
+              }
+            },
+            {"type": "sizedBox", "height": 10},
+            {
+              "type": "expanded",
+              "child": {
+                "type": "text",
+                "data": "%v",
+                "maxLines": 2,
+                "overflow": "ellipsis",
+                "style": {
+                  "fontSize": 14,
+                  "fontWeight": "w700",
+                  "color": "#222222"
+                }
+              }
+            },
+            {"type": "sizedBox", "height": 10}
+          ]
+        }
+      }
+    ]
+  }
+}`
+
+	pdfMap := map[string]any{
+		"type":    "pdf",
+		"route":   "/pdfViewerScreen",
+		"data":    model.Get.Content[0].URL,
+		"content": fmt.Sprintf(str, model.Get.Content[0].Image, model.Get.Content[0].Title),
+	}
+	return pdfMap
+}
+
+func RenderBlog(model appmodels.ContentModel) map[string]any {
+	str := `
+	    {
+  "type": "container",
+  "height": 200,
+  "width": 160,
+  "decoration": {
+    "color": "#FFFFFF",
+    "borderRadius": {
+      "topLeft": 10,
+      "topRight": 10,
+      "bottomLeft": 10,
+      "bottomRight": 10
+    },
+    "boxShadow": [
+      {
+        "color": "#22000000",
+        "blurRadius": 8,
+        "offset": {"dx": 0, "dy": 2}
+      }
+    ]
+  },
+  "child": {
+    "type": "row",
+    "mainAxisSize": "min",
+    "children": [
+      {"type": "sizedBox", "width": 5},
+      {
+        "type": "sizedBox",
+        "width": 150,
+        "child": {
+          "type": "column",
+          "crossAxisAlignment": "start",
+          "children": [
+            {"type": "sizedBox", "height": 10},
+            {
+              "type": "container",
+              "height": 90,
+              "clipBehavior": "hardEdge",
+              "decoration": {
+                "color": "#F0F0F0",
+                "borderRadius": {
+                  "topLeft": 10,
+                  "topRight": 10,
+                  "bottomLeft": 10,
+                  "bottomRight": 10
+                }
+              },
+              "child": {
+                "type": "image",
+                "imageType": "network",
+                "src":
+                    "%v",
+                "fit": "cover",
+                "width": 150,
+                "height": 200
+              }
+            },
+            {"type": "sizedBox", "height": 10},
+            {
+              "type": "container",
+              "decoration": {
+                "color": "#3d1bd1",
+                "borderRadius": {
+                  "topLeft": 8,
+                  "topRight": 8,
+                  "bottomLeft": 8,
+                  "bottomRight": 8
+                }
+              },
+              "child": {
+                "type": "row",
+                "mainAxisSize": "min",
+                "children": [
+                  {"type": "sizedBox", "width": 5},
+                  {
+                    "type": "column",
+                    "children": [
+                      {"type": "sizedBox", "height": 5},
+                      {
+                        "type": "text",
                         "data": "Blog",
                         "style": {"fontSize": 12, "color": "#FFFFFF"}
                       },
@@ -176,7 +306,7 @@ func RenderBlog(model appmodels.ContentModel) map[string]any {
 
 	blogMap := map[string]any{
 		"type":    "blog",
-		"route":   "/blogs",
+		"route":   "/blogScreen",
 		"data":    model.Get.Content[0].URL,
 		"content": fmt.Sprintf(str, model.Get.Content[0].Image, model.Get.Content[0].Title),
 	}
@@ -245,7 +375,7 @@ func RenderAudio(model appmodels.ContentModel) map[string]any {
             {
               "type": "container",
               "decoration": {
-                "color": "#2196F3",
+                "color": "#a2eb50",
                 "borderRadius": {
                   "topLeft": 8,
                   "topRight": 8,
@@ -300,12 +430,133 @@ func RenderAudio(model appmodels.ContentModel) map[string]any {
 
 	audioMap := map[string]any{
 		"type":    "audio",
-		"route":   "/audio",
+		"route":   "/audioPlayerScreen",
 		"data":    model.Get.Content[0].URL,
 		"content": fmt.Sprintf(str, model.Get.Content[0].Image, model.Get.Content[0].Title),
 	}
 	return audioMap
 }
-func RenderVideo() map[string]any {
-	return map[string]any{}
+
+func RenderVideo(model appmodels.ContentModel) map[string]any {
+	str := `
+ {
+  "type": "container",
+  "height": 200,
+  "width": 160,
+  "decoration": {
+    "color": "#FFFFFF",
+    "borderRadius": {
+      "topLeft": 10,
+      "topRight": 10,
+      "bottomLeft": 10,
+      "bottomRight": 10
+    },
+    "boxShadow": [
+      {
+        "color": "#22000000",
+        "blurRadius": 8,
+        "offset": {"dx": 0, "dy": 2}
+      }
+    ]
+  },
+  "child": {
+    "type": "row",
+    "mainAxisSize": "min",
+    "children": [
+      {"type": "sizedBox", "width": 5},
+      {
+        "type": "sizedBox",
+        "width": 150,
+        "child": {
+          "type": "column",
+          "crossAxisAlignment": "start",
+          "children": [
+            {"type": "sizedBox", "height": 10},
+            {
+              "type": "container",
+              "height": 90,
+              "clipBehavior": "hardEdge",
+              "decoration": {
+                "color": "#F0F0F0",
+                "borderRadius": {
+                  "topLeft": 10,
+                  "topRight": 10,
+                  "bottomLeft": 10,
+                  "bottomRight": 10
+                }
+              },
+              "child": {
+                "type": "image",
+                "imageType": "network",
+                "src":
+                    "%v",
+                "fit": "cover",
+                "width": 150,
+                "height": 200
+              }
+            },
+            {"type": "sizedBox", "height": 10},
+            {
+              "type": "container",
+              "decoration": {
+                "color": "#f56642",
+                "borderRadius": {
+                  "topLeft": 8,
+                  "topRight": 8,
+                  "bottomLeft": 8,
+                  "bottomRight": 8
+                }
+              },
+              "child": {
+                "type": "row",
+                "mainAxisSize": "min",
+                "children": [
+                  {"type": "sizedBox", "width": 5},
+                  {
+                    "type": "column",
+                    "children": [
+                      {"type": "sizedBox", "height": 5},
+                      {
+                        "type": "text",
+                        "data": "Video",
+                        "style": {"fontSize": 12, "color": "#FFFFFF"}
+                      },
+                      {"type": "sizedBox", "height": 5}
+                    ]
+                  },
+                  {"type": "sizedBox", "width": 5}
+                ]
+              }
+            },
+            {"type": "sizedBox", "height": 10},
+            {
+              "type": "expanded",
+              "child": {
+                "type": "text",
+                "data": "%v",
+                "maxLines": 2,
+                "overflow": "ellipsis",
+                "style": {
+                  "fontSize": 14,
+                  "fontWeight": "w700",
+                  "color": "#222222"
+                }
+              }
+            },
+            {"type": "sizedBox", "height": 10}
+          ]
+        }
+      }
+    ]
+  }
+}
+  `
+
+	videoMap := map[string]any{
+		"type":    "Video",
+		"route":   "/videoPlayerScreen",
+		"data":    model.Get.Content[0].URL,
+		"content": fmt.Sprintf(str, model.Get.Content[0].Image, model.Get.Content[0].Title),
+	}
+	return videoMap
 }
